bb85bde10a7d8cb4e7f23cdf1066ebb22c207e23
Sasha <sashagrigorev920@gmail.com>
2019-12-10 18:46:46 +0300

    first_commit

---
+++ digits_with_nm.py
@@ -0,0 +1,115 @@
+#!usr/bin/env python
+import sys 
+Zero  = [" *** ", 
+         "*   *", 
+         "*   *", 
+         "*   *",
+         "*   *",
+         "*   *",
+         " *** "]
+One   = [" * ",
+         "** ",
+         " * ",
+         " * ",
+         " * ",
+         " * ",
+         "***"]
+Two   = [" *** ",
+         "*   *",
+         "*  * ",
+         "  *  ",
+         " *   ",
+         "*    ",
+         "*****"]
+Three = [" *** ",
+         "*   *",
+         "    *",
+         "  ** ",
+         "    *",
+         "*   *",
+         " *** "]
+Four  = ["    * ",
+         "   ** ",
+         "  * * ",
+         " *  * ",
+         "******",
+         "    * ",
+         "    * "]
+Five  = ["*****",
+         "*    ",
+         "*    ",
+         " *** ",
+         "    *",
+         "*   *",
+         " *** "]
+Six   = [" *** ",
+         "*   *",
+         "*    ",
+         "**** ",
+         "*   *",
+         "*   *",
+         " *** "]
+Seven = ["*****",
+         "    *",
+         "   * ",
+         "  *  ",
+         " *   ",
+         "*    ",
+         "*    "]
+Eight = [" *** ",
+         "*   *",
+         "*   *",
+         " *** ",
+         "*   *",
+         "*   *",
+         " *** "]
+Nine  = [" ****",
+         "*   *",
+         "*   *",
+         " ****",
+         "    *",
+         "    *",
+	 " *** "]
+
+
+Digits= [Zero, One, Two, Three, Four,Five, Six,Seven, Eight, Nine]
+
+try:
+	number=sys.argv[1]
+	k=0
+	while k<7:
+		line=""
+		leng=0
+		while leng<len(number):
+			chislo=Digits[int(number[leng])]
+			line+=chislo[k].replace('*',number[leng])+' '
+			leng+=1
+		print(line)
+		k+=1
+
+
+except IndexError:
+    print("Usage: bigdigits.py <numbers>")
+except ValueError as err:
+    print(err, "in", digits)
+			
+				
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			
